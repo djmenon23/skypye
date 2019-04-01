58ce2a11abb63073223cc688a68522ecd1b002fe
Dhananjay Menon <Dhananjay.Menon@cognizant.com>
2019-04-01 15:16:29 +0530

    finished the application

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -3,15 +3,18 @@ import store from "./../store/index";
 import Header from './../components/Header';
 import  Chats  from '../components/Chats';
 import _ from 'lodash';
+import MessageInput from "./MessageInput";
 
 const ChatWindow = ({ activeUserId }) => {
 const state = store.getState();
+const { typing } = state;
 const activeUser = state.contacts.contacts[activeUserId];
 const messages = state.messages[activeUserId];
 return (
 <div className="ChatWindow">
 <Header user={activeUser} />
 <Chats messages={_.values(messages)} />
+<MessageInput value={typing} />
 </div>
 );
 };
